f8fdf2a1f108350079a6c8cc0ade1e223d55a359
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-11 05:56:57 +0200

    feat: FEEEEEEEEEEEEEEATTTTTTTTTTTTTTTTTTTT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
 import Image from "next/image";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -8,6 +22,23 @@ export default async function Home() {
         PLACE IS A WARZONE. NOTHING WORKS, EVERYTHING BREAKS. THIS IS THE PLACE
         WHERE EVERYTHING BREAKS.{" "}
       </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      <Popover>
+        <PopoverTrigger>Open</PopoverTrigger>
+        <PopoverContent>Place content for the popover here.</PopoverContent>
+      </Popover>
+      <div></div>
     </main>
   );
 }
