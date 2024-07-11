ebc2bfabef8fa346ed8f1e834f693c73f7d19187
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-11 08:26:36 +0200

    feat: BRING BACK MARQUEE

--- src/app/warzone/page.tsx
+++ src/app/warzone/page.tsx
@@ -1,7 +1,15 @@
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       AAAAAAAAAAAAAAAAAAAAAAAAAA
+      <div className=" bg-red-300 h-[100px] w-[200px]">
+        <marquee>AaaaaaaaaaaAAAAAAAAAAAaaaAAaaaAAAAAa</marquee>
+      </div>
+      <div className="h-[300px] w-[300px]">
+        <marquee direction="down" width="250" height="200" behavior="alternate">
+          <marquee behavior="alternate">#BRING BACK MARQUEE</marquee>
+        </marquee>
+      </div>
     </main>
   );
 }
