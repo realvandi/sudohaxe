6ff7afb94db4de12a3da464e883612673737cdd5
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-11 04:47:03 +0200

    feat: FEAT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello. This is sudohaxe.com!</div>
+      <div>
+        Hello. This is sudohaxe.com! THIS IS A HIGHLY EXPERIMENTAL SITE. THIS
+        PLACE IS A WARZONE. NOTHING WORKS, EVERYTHING BREAKS. THIS IS THE PLACE
+        WHERE EVERYTHING BREAKS.{" "}
+      </div>
     </main>
   );
 }
