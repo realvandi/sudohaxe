1109d6a116b00cab64f36fc95e45b130ad6e8c0e
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-11 07:46:39 +0200

    feat: Feat

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/sudohaxe",
+  // process.env.NEXT_PUBLIC_NODE_ENV === "PRODUCTION" ? "/sudohaxe" : "", //DUE TO GH PAGES
 };
 
 export default nextConfig;
