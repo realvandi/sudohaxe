80b4a916f7d44d7f6b08fdc6f28cc48c94903e55
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-11 02:16:44 +0200

    feat: Initialize NextJS project

---
+++ sudohaxe/next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+//GitHub pages config example from FreeCodeCamp
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: "/sudohaxe",
+//   output: "export",
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
