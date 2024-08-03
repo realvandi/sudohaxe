009992b0890d45495d79c8415fac352c7a590da0
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-08-03 22:11:37 +0200

    feat: Change home page link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,22 +29,24 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://tickables.com"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Tickables.com
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            {"You can check this out"}
-          </p>
-        </a>
+        {
+          //<a
+          //  href="https://tickables.com"
+          //  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          //  target="_blank"
+          //  rel="noopener noreferrer"
+          //>
+          //  <h2 className="mb-3 text-2xl font-semibold">
+          //    Tickables.com
+          //    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+          //      -&gt;
+          //    </span>
+          //  </h2>
+          //  <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          //    {"You can check this out"}
+          //  </p>
+          //</a>
+        }
 
         <a
           href="https://sudohaxe.com"
